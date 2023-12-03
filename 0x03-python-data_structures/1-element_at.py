7c07e999181e1ab00f7c432d7fb73c828aa05193
WayneRu95 <waynerumbidzai@gmail.com>
2023-12-03 13:26:19 -0800

    Correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/bin/bash/python3
 def element_at(my_list, idx):
-    ***Function that retrieves an element from a list like in C***
     for x in my_list:
         if idx == my_list.index(x):
             return(x)
